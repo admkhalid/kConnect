b0e3071ebb18723aefef13f45bf4b0f5e8eaef0f
Kalid Ahamed <admkhalid@zoho.com>
2019-09-08 11:01:23 +0530

    migrated and created db with models; added some data to db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    # date_posted = models.DateTimeField(auto_now = True) - update the date whenever the post is modified
+    # date_posted = models.DateTimeField(auto_now_add = True) - keeps the date of the time when this object was created. 
+    # doesn't let you change the date
+    date_posted = models.DateTimeField(default=timezone.now) #now is a function. don't include the paranthesis, otherwise it'll execute.
+    author = models.ForeignKey(User, on_delete = models.CASCADE) #on_delete = models.CASCADE deletes the post of the user is deleted
+    
+    #this prints the title of the object instead of the object id in the prompt 
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def home(request):
-    posts = [
-        {
-            'title': 'Post1',
-            'author': 'admkhalid',
-            'date_posted': '28 June 2019',
-            'content': 'dummy content'
-        },
-        {
+    # posts = [
+    #     {
+    #         'title': 'Post1',
+    #         'author': 'admkhalid',
+    #         'date_posted': '28 June 2019',
+    #         'content': 'dummy content'
+    #     },
+    #     {
             
-            'title': 'Post2',
-            'author': 'Jane Doe',
-            'date_posted': '29 June 2019',
-            'content': 'dummy content 2'
-        }
-    ]
+    #         'title': 'Post2',
+    #         'author': 'Jane Doe',
+    #         'date_posted': '29 June 2019',
+    #         'content': 'dummy content 2'
+    #     }
+    # ]
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
