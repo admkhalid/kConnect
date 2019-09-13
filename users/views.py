6e9ff8725d0b8db537261071861f25b2bf6e18d4
Kalid Ahamed <admkhalid@zoho.com>
2019-09-13 16:28:29 +0530

    a view for user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+#django can generate its own html user creation form which handles all the validations(regex etc.)
+from django.contrib import messages #create flash messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save() #yeah simple as that. the user created and saved in the database
+            username = form.cleaned_data.get('username') #cleaned_data is a dictionary
+            # messages.success
+            # messages.error
+            # messages.warning
+            # messages.info
+            # messages.DEBUG
+            message = messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
