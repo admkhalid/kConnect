896f778a40feec582a9ac3678f46f57dc08b4c4c
Kalid Ahamed <admkhalid@zoho.com>
2019-09-13 16:48:48 +0530

    created UserRegisterForm class

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField() #default arg is required = TRUE
+    
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'second_name',
+            'email',
+            'password1',
+            'password2'
+            ]
