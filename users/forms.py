1ee039f5eb0322f5ac22732ad1b2d6d9607bd3d1
Kalid Ahamed <admkhalid@zoho.com>
2019-09-13 17:16:30 +0530

    commented that i can create a new field

--- users/forms.py
+++ users/forms.py
@@ -4,13 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField() #default arg is required = TRUE
-    
+    # name = forms.CharField()you can create a new field
     class Meta:
         model = User
         fields = [
             'username',
             'first_name',
-            'second_name',
+            'last_name',
+            # 'name',
             'email',
             'password1',
             'password2'
