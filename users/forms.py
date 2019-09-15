6b45d31fd54f0fbbc55d69d820e7ac03ab725034
Kalid Ahamed <admkhalid@zoho.com>
2019-09-15 15:46:04 +0530

    added functionality to update profile details

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField() #default arg is required = TRUE
@@ -16,3 +17,19 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2'
             ]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            ]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
