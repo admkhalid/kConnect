2843309bcb02a761d16d9e38059275aec088edf2
Kalid Ahamed <admkhalid@zoho.com>
2019-09-21 23:18:58 +0530

    spacing

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField() #default arg is required = TRUE
+    email = forms.EmailField()  # default arg is required = TRUE
+
     # name = forms.CharField()you can create a new field
     class Meta:
         model = User
@@ -16,10 +18,12 @@ class UserRegisterForm(UserCreationForm):
             'email',
             'password1',
             'password2'
-            ]
+        ]
+
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = [
@@ -27,7 +31,8 @@ class UserUpdateForm(forms.ModelForm):
             'first_name',
             'last_name',
             'email',
-            ]
+        ]
+
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
